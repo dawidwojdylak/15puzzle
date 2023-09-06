a2a1a126d0eb91b54c4201a622fe2bab685400d9
dawidwojdylak <dawidwojdylak98@gmail.com>
2023-09-06 19:32:19 +0200

    Fix crash

--- piece.cpp
+++ piece.cpp
@@ -5,8 +5,10 @@ Piece::Piece(int id, const QPixmap & img) : m_id{id}, m_image{img}
     setPixmap(m_image);
 }
 
+Piece::Piece(const Piece &other)
+: m_id{other.m_id}, m_image{other.m_image} {}
+
 void Piece::mousePressEvent(QMouseEvent *event)
 {
     emit moveSelf(m_id, true);
-    QLabel::mousePressEvent(event);
 }
